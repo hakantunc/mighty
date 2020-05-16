28605f78f2aef2c25b263cc1afa5cdbc95a5bf01
Hakan Tunc <hakan.tunc@live.com>
2020-05-15 21:46:30 -0700

    Updates

---
+++ precache-manifest.7cb9d31322ba64932939099dd6f35aff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e79852e2bd158deaa0cb4070a3ba6b",
+    "url": "/mighty/index.html"
+  },
+  {
+    "revision": "228dc348a5b1c7acf648",
+    "url": "/mighty/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e759ffc9683ae01bf454",
+    "url": "/mighty/static/css/main.e0939ce3.chunk.css"
+  },
+  {
+    "revision": "228dc348a5b1c7acf648",
+    "url": "/mighty/static/js/2.0a84ed1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mighty/static/js/2.0a84ed1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e759ffc9683ae01bf454",
+    "url": "/mighty/static/js/main.c77c1eb7.chunk.js"
+  },
+  {
+    "revision": "91722ed6f1c9c7b39350",
+    "url": "/mighty/static/js/runtime-main.8fd9c330.js"
+  }
+]);
